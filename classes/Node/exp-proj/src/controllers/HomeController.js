72d6b30e3ec0c0ea729190a5feda69636d95824f
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-28 21:36:08 -0300

    ✨ Add HomeController

--- classes/Node/exp-proj/src/controllers/HomeController.js
+++ classes/Node/exp-proj/src/controllers/HomeController.js
@@ -1,9 +1,26 @@
+const HomeModel = require('../models/HomeModel');
+
 module.exports = {
   async index(request, response) {
     return response.render('index');
+    /* Coloque next(); caso haja outro middleware após esse controller */
   },
 
   async store(request, response) {
-    return response.json('Rota de criação.')
+    const { title, description } = request.body;
+    
+    try {
+      const data = await HomeModel.create({
+        title,
+        description,
+      });
+
+      return response.status(201).send(data);
+
+    } catch (err) {
+      return response.status(400).send('Error on creation: ' + err);
+    }
+    
+    /* Coloque next(); caso haja outro middleware após esse controller */
   }
 };
