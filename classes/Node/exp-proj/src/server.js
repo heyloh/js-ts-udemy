eafae29e955bcf8333d1099ad242d50fd44468f8
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-28 21:35:57 -0300

    ✨ Update server.js

--- classes/Node/exp-proj/src/server.js
+++ classes/Node/exp-proj/src/server.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('✅ Connection to the database established.');
+    app.emit('Ready!');
+  })
+  .catch(e => {
+    console.log('🚨 Error connecting to the database: ', e);
+  });
 
 app.set('views', path.resolve(__dirname, 'views')); // Onde buscar por Views
 app.set('view engine', 'ejs'); // O que vamos usar para renderizar as Views
@@ -11,7 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('🚀 Server listening on: 3333');
-  console.log('🚧 URL: http://localhost:3333/');
+app.on('Ready!', () => {
+  app.listen(3333, () => {
+    console.log('🚀 Server listening on: 3333');
+    console.log('🚧 URL: http://localhost:3333/');
+  });
 });
